8394b3a6f8a9ba02ad496409867c8e125320014a
Julka <julialtnc@gmail.com>
2023-10-29 13:50:53 +0100

    Added option fields for frames

--- multiplication_table_task/app.py
+++ multiplication_table_task/app.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    table = None
+    x, color1, color2, table, with_border = None, None, None, '', False
     if request.method == 'POST':
         x = int(request.form['x'])
         color1 = request.form['color1']
         color2 = request.form['color2']
-        table = generate_table(x, color1, color2)
-    return render_template('form.html', table=table)
+        with_border = request.form.get('ramka') == 'border'
+        table = generate_table(x, color1, color2, with_border)
+    return render_template('form.html', table=table, x=x, color1=color1, color2=color2, with_border=with_border)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- multiplication_table_task/multiplication_table.py
+++ multiplication_table_task/multiplication_table.py
@@ -1,13 +1,11 @@
-def generate_table(size, color1, color2):
-    table = '<table border align=center>'
-    color = 'white'
+def generate_table(size, color1, color2, with_border):
+    table = f"<table{' border' if with_border else ''} align='center'>"
+    colors = [color1, color2]
     for i in range(1, size + 1):
+        table += '<tr>'
         for j in range(1, size + 1):
-            if i * j % 2 == 0:
-                color = color1
-            else:
-                color = color2
-            table += f"<td bgcolor={color}>{i * j}</td>"
+            color_index = (i * j) % 2
+            table += f'<td bgcolor={colors[color_index]}>{i * j}</td>'
         table += '</tr>'
     table += '</table>'
     return table
